14cd10865eb4372b768e992dcb9a402a0e623373
sumesh7353511222 <56567860+sumesh7353511222@users.noreply.github.com>
2019-10-19 11:48:22 +0530

    first commit

---
+++ MVc1/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using MVc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVc1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            //ContactViewModel obj = new ContactViewModel();
+            //obj.FName = "SUMESH";
+            //obj.LName = "Nammilikkara";
+            ContactViewModel obj1 = new ContactViewModel()
+            {
+                FName = "Vedha",
+                LName = "Sumesh"
+            };
+
+             
+
+            return View(obj1);
+        }
+    }
+}
---
+++ MVc1/Models/ContactViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVc1.Models
+{
+    public class ContactViewModel
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
+}
